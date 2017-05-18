6745b43550059af7094a6fc10145e373c5c2d3e4
Json-Lin <447995687@qq.com>
2017-05-18 09:36:11 +0800

    xxx

--- src/main/java/com/test/WantToTestConfig.java
+++ src/main/java/com/test/WantToTestConfig.java
@@ -1,5 +1,10 @@
 package com.test;
 
 public class WantToTestConfig {
+	
+	private static String out = "test config"; 
 
+	public static void main(String[] args) {
+		System.out.println(out);
+	}
 }
