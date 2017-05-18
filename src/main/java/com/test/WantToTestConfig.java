c534eefb2e1cf0379eb1b35a70764ef55032cf7b
Velen <447995687@qq.com>
2017-05-18 09:40:40 +0800

    Update WantToTestConfig.java

--- src/main/java/com/test/WantToTestConfig.java
+++ src/main/java/com/test/WantToTestConfig.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class WantToTestConfig {
 	
-	private static String out = "test config"; 
+	private static String out = "test ttox"; 
 
 	public static void main(String[] args) {
 		System.out.println(out);
